e30785ffc5728810b879c7de093af5227d220083
vagrant <vagrant@homestead>
2017-10-09 16:24:57 +0000

    确认密码验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,8 +21,7 @@ class UsersController extends Controller
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
-            'password' => 'required',
-            'password_confirmation' => 'confirmed'
+            'password' => 'required|confirmed'
         ]);
 
         $user = User::create([
